fafa2ede581e572fc113908bac501410be75f78e
Rithik Rawat <124555773+rithikrawat@users.noreply.github.com>
2024-05-04 22:02:17 +0530

    Game

---
+++ Pig Game/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23 , Firenze , Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegatarian', 'Organic'] ,
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'] ,
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+};
